9cbb904200460c1176d7e469d3feae7635ab91ef
JalalHasan-22 <jhasanj8@gmail.com>
2022-05-21 20:54:11 +0300

    organized the folder structure

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,16 @@
 import Card from '../components/shared/Card';
 import { Link } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
 
 function About() {
   return (
     <Card>
       <h1>About this project</h1>
       <p>This is a simple react.js app to leave a feedback</p>
-      <Link to='/'>Back to home page</Link>
+      <br />
+      <Link to='/' className='about-link'>
+        {<FaHome size={30} color='#fff' />}
+      </Link>
     </Card>
   );
 }
