ac06dfbe374167e23013900ca6395cbea20fc5ef
JalalHasan-22 <jhasanj8@gmail.com>
2022-05-21 20:40:39 +0300

    Added Context Provider

--- feedback-app/src/components/FeedbackItem.jsx
+++ feedback-app/src/components/FeedbackItem.jsx
@@ -1,24 +1,23 @@
 import Card from './shared/Card';
-import PropTypes from 'prop-types';
-import { FaTimes } from 'react-icons/fa';
+import { useContext } from 'react';
+import FeedbackContext from '../context/FeedbackContext';
+import { FaTimes, FaEdit } from 'react-icons/fa';
 
-function FeedbackItem({ item, deleteFeedback }) {
-  const handleDelete = (id) => {
-    deleteFeedback(id);
-  };
+function FeedbackItem({ item }) {
+  const { deleteFeedback, editFeedback } = useContext(FeedbackContext);
+
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)} className='close'>
+      <button onClick={() => deleteFeedback(item.id)} className='close'>
         <FaTimes color='#E12901' />
+      </button>
+      <button className='edit' onClick={() => editFeedback(item)}>
+        <FaEdit color='Green' />
       </button>
       <div className='text-display'>{item.text}</div>
     </Card>
   );
 }
-
-FeedbackItem.propTypes = {
-  item: PropTypes.object,
-};
 
 export default FeedbackItem;
